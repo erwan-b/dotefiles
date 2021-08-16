3c6737941ed5a0a9a8a3346553590d2ddb4028b2
Erwan Bernard <erwan.bernard079@gmail.com>
2021-08-16 13:45:20 +0200

    update ansible script and zsh

--- polybare/launch.sh
+++ polybare/launch.sh
@@ -3,7 +3,7 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 if type "xrandr"; then
-  for m in $(xrandr --query | grep "connected" | cut -d" " -f1); do
+  for m in $(polybar --list-monitors | cut -d":" -f1); do
     MONITOR=$m polybar --reload white &
   done
 else
